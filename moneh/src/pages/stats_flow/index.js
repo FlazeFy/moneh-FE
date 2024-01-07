e72db8498c298244afb2f3726e6abfd2f9778af3
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-01-07 23:34:54 +0700

    get total flow by cat chart

--- moneh/src/pages/stats_flow/index.js
+++ moneh/src/pages/stats_flow/index.js
@@ -1,14 +1,17 @@
 import GetNavbar from "../../components/bars/navbar";
-import GetTotalFlowByType from "./usecases/getTotalFlowByType";
+import GetTotalFlowByType from "./usecases/get_total_flow_by_type";
+import GetTotalFlowByCategory from "./usecases/get_total_flow_by_category";
 
 const StatsFlow_Index = () => {
     return (
         <div className='content-grid'>
             <GetNavbar active="flow"/>
-            <div className="row">
+            <div className="row mt-5">
                 <div className="col-lg-6 col-md-6 col-sm-12">
                     <GetTotalFlowByType ctx="get_total_flow_by_type"/>
-                    
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <GetTotalFlowByCategory ctx="get_total_flow_by_category"/>
                 </div>
             </div>
         </div>
