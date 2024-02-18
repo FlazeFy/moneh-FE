957db5b78f2207a51bbab6363385f4185102888f
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-02-18 15:27:10 +0700

    get sign out

--- moneh/src/components/bars/navbar.js
+++ moneh/src/components/bars/navbar.js
@@ -1,9 +1,4 @@
-//Font awesome classicon
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightFromBracket, faXmark} from "@fortawesome/free-solid-svg-icons"
-import { getLocal } from "../../modules/storages/local";
-import { isLogged } from "../../modules/helpers/auth";
+import GetSignOut from "./usecases/get_sign_out";
 
 export default function GetNavbar({active, subactive}) {
     function getActive(val, curr){
@@ -33,11 +28,7 @@ export default function GetNavbar({active, subactive}) {
                     </div>
                 </li>
                 <li><a className={"nav-link " + getActive(active,"about")} href="/about">About</a></li>
-                <li>
-                    <a className={"nav-link " + isLogged('bg-danger text-white', 'bg-success text-white') + getActive(active,"login")} href="/login">
-                        {isLogged(<FontAwesomeIcon icon={faXmark} size="2xl"/>, <FontAwesomeIcon icon={faRightFromBracket} size="xl"/>)}
-                    </a>
-                </li>
+                <GetSignOut active={active}/>
             </ul>
             <i className="fa-solid fa-bars mobile-nav-toggle" title="Open NavBar"></i>
         </nav>
---
+++ moneh/src/components/bars/usecases/get_sign_out.js
@@ -0,0 +1,80 @@
+import Axios from 'axios'
+import { useState } from 'react'
+
+
+//Font awesome classicon
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightFromBracket, faXmark} from "@fortawesome/free-solid-svg-icons"
+
+// Module
+import { getLocal } from "../../../modules/storages/local";
+import { isLogged } from "../../../modules/helpers/auth";
+
+// Component
+import modal from '../../../components/modals/modals.module.css'
+
+export default function GetSignOut({active}) {
+    const keyToken = getLocal("token_key")
+    const [resMsgAll, setResMsgAll] = useState("")
+
+    function getActive(val, curr){
+        if(val == curr){
+            return "active";
+        } else {
+            return "";
+        }
+    }
+
+    // Services
+    const handleSubmit = async (e) => {
+        try {
+            const response = await Axios.post("http://127.0.0.1:1323/api/v1/logout", null, {
+                headers: {
+                    Authorization: `Bearer ${keyToken}`
+                }
+            })
+            if(response.status != 200){
+                window.location.reload(false)
+                return response.data.message
+            } else {
+                localStorage.clear()
+                window.location.href = '/'
+            }
+        } catch (error) {
+            setResMsgAll(error)
+        }
+    }
+
+    return (
+        <li>
+            {
+                isLogged(
+                    <>
+                        <a className={"nav-link bg-danger text-white" + getActive(active,"login")} data-bs-toggle="modal" data-bs-target={"#signoutModal"}>
+                            <FontAwesomeIcon icon={faXmark} size="2xl"/>
+                        </a>
+                        <div className="modal fade" id={"signoutModal"} aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="exampleModalLabel">Sign Out</h5>
+                                        <button type="button" className={modal.btn_close_modal} data-bs-dismiss="modal" aria-label="Close"><FontAwesomeIcon icon={faXmark}/></button>
+                                    </div>
+                                    <div className="modal-body text-center p-4">
+                                        <p className="text-white mt-4">Are you sure want to sign out from this account?</p>
+                                        <button className="btn btn-danger w-100 mb-4" onClick={handleSubmit}>Yes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                    ,
+                    <a className={"nav-link bg-success text-white" +  getActive(active,"login")} href="/login">
+                        <FontAwesomeIcon icon={faRightFromBracket} size="xl"/>
+                    </a>
+                )
+            }
+        </li>
+    )
+}
